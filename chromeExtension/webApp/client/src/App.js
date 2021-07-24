acbd581fd7ea0a26a91a686c1521fdac56160ab6
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-07-25 05:42:38 +0900

    make react app

--- chromeExtension/webApp/client/src/App.js
+++ chromeExtension/webApp/client/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 
 function App() {
-  let bibibibi = '뭐라구 안들리는데';
-  function test1() {
-    return '잘들리는데??'
-  }
+  // let bibibibi = '뭐라구 안들리는데';
+  // function test1() {
+  //   return '잘들리는데??'
+  // }
+  let post = 'title';
   return (
     <div className="App">
       <div className="black-nav">
         SeeUrLater
       </div>
-      <h4>{bibibibi}</h4>
-      {test1()}
+      {/* <h4>{bibibibi}</h4>
+      {test1()} */}
+      <div className="list">
+        <h3>{ post }</h3>
+        <p>content</p>
+        <hr/>
+      </div>
     </div>
   );
 }
