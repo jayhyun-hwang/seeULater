45d39868e08626a42f6d87b90d69d442d82f9177
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-07-25 02:57:31 +0900

    Orgarnize directory

---
+++ chromeExtension/webApp/client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  let bibibibi = '뭐라구 안들리는데';
+  function test1() {
+    return '잘들리는데??'
+  }
+  return (
+    <div className="App">
+      <div className="black-nav">
+        SeeUrLater
+      </div>
+      <h4>{bibibibi}</h4>
+      {test1()}
+    </div>
+  );
+}
+
+export default App;
