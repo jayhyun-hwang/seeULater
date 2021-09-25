0d4232a8a87ed59e049aaa731eed6342d3ff5897
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-09-25 21:57:49 +0900

    debug batch

---
+++ runServersDebug.bat
@@ -0,0 +1,3 @@
+cd C:\Users\wogus\AppData\Local\Programs\Microsoft VS Code\
+start .\Code.exe C:\GoWorkspace\src\seeULater\webApp\nodeServer
+start .\Code.exe C:\GoWorkspace\src\seeULater\webApp\sulClient
