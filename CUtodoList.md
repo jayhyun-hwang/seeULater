f82d1aab4f6a3a5da3cc79e6707f87c1cb9fe1f1
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-09-27 02:14:28 +0900

    add todolist, sql script

--- CUtodoList.md
+++ CUtodoList.md
@@ -1,13 +1,20 @@
 # Major(step)
 
+- aws에 저장소 올려놓기
+- url, title, icon 모두 저장. 보여주기
 - **caching preview information to localstorage**
-- 로고 악어로 바꾸기
+- 계정기능, 쿠키값으로 사용자 식별 (jwt 사용)
+- 크롬 확장기능을 통해 저장했을 때, 소켓으로 페이지 새로고침하기. (websocket 사용)
+- 파이어베이스 계정기능
+- 파일 디렉토리기능 구현, div 추가
+- 그룹 만들어 공동관리 가능
+- ~~로고 악어로 바꾸기~~
 - ~~store url permanently~~
 - ~~make url list - page~~
-- make a managing page
-  - CRUD urls
+- ~~make a managing page~~
+  - CRUD urls, **read 상태 업데이트 관리 남음**
   - enable to make tree directory(category folder structure)
-- when click extension icon, show managing menu
+- ~~when click extension icon, show managing menu~~
   - move to a managing page
   - Store this url (when the page blocked using right click)
 
@@ -15,4 +22,5 @@
 
 - when stored, popup at right bottom of the page(tab) - instead alert
 
+- 메인로고에 악어 넣기, 메인으로 이동
 - 
--- webApp/nodeServer/seeUscript.sql
+++ webApp/nodeServer/seeUscript.sql
@@ -18,12 +18,27 @@ DROP DATABASE IF EXISTS `seeulater`;
 CREATE DATABASE IF NOT EXISTS `seeulater` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
 USE `seeulater`;
 
+-- 테이블 seeulater.system_option 구조 내보내기
+DROP TABLE IF EXISTS `system_option`;
+CREATE TABLE IF NOT EXISTS `system_option` (
+  `version` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- 테이블 데이터 seeulater.system_option:~0 rows (대략적) 내보내기
+DELETE FROM `system_option`;
+/*!40000 ALTER TABLE `system_option` DISABLE KEYS */;
+INSERT INTO `system_option` (`version`) VALUES
+	(1);
+/*!40000 ALTER TABLE `system_option` ENABLE KEYS */;
+
 -- 테이블 seeulater.urls 구조 내보내기
 DROP TABLE IF EXISTS `urls`;
 CREATE TABLE IF NOT EXISTS `urls` (
   `url_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `url` text NOT NULL,
+  `title` text DEFAULT NULL,
+  `icon_img` text DEFAULT NULL,
   `description` text DEFAULT NULL,
   `memo` text DEFAULT NULL,
   `directory` int(11) DEFAULT NULL,
@@ -33,17 +48,28 @@ CREATE TABLE IF NOT EXISTS `urls` (
   PRIMARY KEY (`url_id`),
   KEY `urls_FK` (`user_id`),
   CONSTRAINT `urls_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb3;
 
--- 테이블 데이터 seeulater.urls:~5 rows (대략적) 내보내기
+-- 테이블 데이터 seeulater.urls:~16 rows (대략적) 내보내기
 DELETE FROM `urls`;
 /*!40000 ALTER TABLE `urls` DISABLE KEYS */;
-INSERT INTO `urls` (`url_id`, `user_id`, `url`, `description`, `memo`, `directory`, `regdate`, `deldate`, `read`) VALUES
-	(1, 1, 'https://jayhyun-hwang.github.io/', NULL, NULL, NULL, '2021-09-21 01:23:41', NULL, 0),
-	(2, 1, 'https://github.com/jayhyun-hwang', NULL, NULL, NULL, '2021-09-21 01:30:19', NULL, 0),
-	(3, 1, '//localhost:3000/', NULL, NULL, NULL, '2021-09-21 03:17:04', NULL, 0),
-	(4, 1, 'https://www.baeldung.com/cs/', NULL, NULL, NULL, '2021-09-21 17:01:23', NULL, 0),
-	(5, 1, 'http://www.danawa.com/', NULL, NULL, NULL, '2021-09-21 17:04:11', '2021-09-21 17:43:32', 0);
+INSERT INTO `urls` (`url_id`, `user_id`, `url`, `title`, `icon_img`, `description`, `memo`, `directory`, `regdate`, `deldate`, `read`) VALUES
+	(1, 1, 'https://jayhyun-hwang.github.io/', NULL, NULL, NULL, NULL, NULL, '2021-09-21 01:23:41', NULL, 0),
+	(2, 1, 'https://github.com/jayhyun-hwang', NULL, NULL, NULL, NULL, NULL, '2021-09-21 01:30:19', NULL, 0),
+	(3, 1, '//localhost:3000/', NULL, NULL, NULL, NULL, NULL, '2021-09-21 03:17:04', '2021-09-26 19:13:59', 0),
+	(4, 1, 'https://www.baeldung.com/cs/', NULL, NULL, NULL, NULL, NULL, '2021-09-21 17:01:23', '2021-09-26 19:18:03', 0),
+	(10, 1, 'https://www.playstation.com/ko-kr/', NULL, NULL, NULL, NULL, NULL, '2021-09-23 22:38:59', '2021-09-26 19:15:50', 0),
+	(11, 1, 'http://golang.site/', NULL, NULL, NULL, NULL, NULL, '2021-09-23 22:39:13', '2021-09-26 19:17:10', 0),
+	(12, 1, 'https://stackoverflow.com/questions/3833561/why-doesnt-git-ignore-my-specified-file/3833675', NULL, NULL, NULL, NULL, NULL, '2021-09-23 22:39:21', '2021-09-26 19:11:10', 0),
+	(13, 1, 'https://store.playstation.com/ko-kr/category/c4113150-fbb2-40ae-8fe8-55cd9c089cd8/1', NULL, NULL, NULL, NULL, NULL, '2021-09-23 22:39:29', '2021-09-26 19:10:40', 0),
+	(14, 1, 'https://store.playstation.com/ko-kr/category/c4113150-fbb2-40ae-8fe8-55cd9c089cd8/1', NULL, NULL, NULL, NULL, NULL, '2021-09-23 22:39:38', '2021-09-26 19:09:49', 0),
+	(16, 1, 'https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send', NULL, NULL, NULL, NULL, NULL, '2021-09-27 00:35:32', '2021-09-27 01:50:24', 0),
+	(17, 1, 'https://stackoverflow.com/questions/17007997/how-to-access-the-get-parameters-after-in-express', NULL, NULL, NULL, NULL, NULL, '2021-09-27 01:40:46', '2021-09-27 01:50:23', 0),
+	(18, 1, 'https://developer.chrome.com/docs/extensions/mv3/manifest/activeTab/', NULL, NULL, NULL, NULL, NULL, '2021-09-27 01:45:35', '2021-09-27 01:50:21', 0),
+	(19, 1, 'https://stackoverflow.com/questions/9713058/send-post-data-using-xmlhttprequest', NULL, NULL, NULL, NULL, NULL, '2021-09-27 01:47:23', '2021-09-27 01:50:18', 0),
+	(20, 1, 'https://multifrontgarden.tistory.com/191', NULL, NULL, NULL, NULL, NULL, '2021-09-27 01:47:38', '2021-09-27 01:50:14', 0),
+	(21, 1, 'https://www.youtube.com/watch?v=glXgSSOKlls', NULL, NULL, NULL, NULL, NULL, '2021-09-27 01:49:35', '2021-09-27 01:50:17', 0),
+	(22, 1, 'http://localhost:3000/', NULL, NULL, NULL, NULL, NULL, '2021-09-27 02:05:46', NULL, 0);
 /*!40000 ALTER TABLE `urls` ENABLE KEYS */;
 
 -- 테이블 seeulater.users 구조 내보내기
