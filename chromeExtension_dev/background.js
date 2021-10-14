9de2375bf343e14753ff90877da2e025941e0a49
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-10-14 23:07:58 +0900

    add shortcut key to chrome extension

--- chromeExtension_dev/background.js
+++ chromeExtension_dev/background.js
@@ -1,13 +1,16 @@
 const define = {
-  URL: "http://127.0.0.1",
-  // URL : "http://3.36.36.62",
+  // URL: "http://3.36.36.62",
+  // id: "storePage",
+  // title: "Store this page",
+  URL : "http://127.0.0.1",
+  id: "storePage_dev",
+  title: "Store this page_dev",
   PORT: 3001
 };
-//----------------------------------------set dev mode-----------------------------------------------
+//----------------------------------------set mode-----------------------------------------------
 
 // const baseUrl = "http://3.36.36.62:3001";
 const baseUrl = define.URL + ':' + define.PORT;
-
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -15,13 +18,6 @@ const baseUrl = define.URL + ':' + define.PORT;
 /**
  * Returns a handler which will open a new window when activated.
  */
-
-chrome.commands.onCommand.addListener(function (command) {
-  console.log('onCommand event received for message: ', command);
-});
-
-
-
 function getStoreClickHandler() {
   return function (info, tab) {
 
@@ -93,9 +89,36 @@ function getStoreClickHandler() {
  * Create a context menu which will only show up for images.
  */
 chrome.contextMenus.create({
-  "id": "storePage",
-  "title": "Store this page_div",
+  "id": define.id,
+  "title": define.title,
   "type": "normal", //"normal", "checkbox", "radio", or "separator"
   "onclick": getStoreClickHandler(),
   "contexts": ["all"]
+});
+
+chrome.commands.onCommand.addListener(function (command, tab) {
+  console.log('onCommand event received for message: ', command);
+  if (command == "store_page") {
+    console.log('in', command);
+    
+    const title = tab.title;
+    const url = tab.url;
+    const iconImg = tab.favIconUrl;
+
+    let req = new XMLHttpRequest();
+
+    req.open("POST", baseUrl + "/urls", true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send(JSON.stringify({
+      url: url,
+      iconImg: iconImg,
+      title: title
+    }));
+
+    req.onreadystatechange = function () { // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        alert("Got response 200!");
+      }
+    }
+  }
 });
