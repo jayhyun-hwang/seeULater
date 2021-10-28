0ccbb1bdf2d118faa72e89e6d70f8022ab4e6119
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-10-28 22:50:20 +0900

    build app for deployee

--- webApp/sulClient/src/define/define.js
+++ webApp/sulClient/src/define/define.js
@@ -2,6 +2,7 @@ module.exports = {
     URL: "http://3.36.36.62",
     // URL: "http://127.0.0.1:3001",
     PORT: 80,
-    REQURL: "http://127.0.0.1:3001"
+    REQURL: "http://3.36.36.62",
+    REQURLDEV: "http://127.0.0.1:3001"
     // REQURL: "http://3.36.36.62:3001"
 }
