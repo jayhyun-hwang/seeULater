428bbde614d00e2dba37da4e4a979e54adad551f
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-10-29 00:01:05 +0900

    build app for deployee

--- webApp/nodeServer/index.js
+++ webApp/nodeServer/index.js
@@ -108,8 +108,8 @@ app.post('/login',(req, res) => {
 
 //make router, req handlers
 app.post('/urls', (req, res) => {
-    console.log(req);
-    console.log(req.body);
+    // console.log(req);
+    // console.log(req.body);
     // 사용자는 쿠키로 확인
     // const userID = req.body.userID;
     const userID = 1;   // dev
