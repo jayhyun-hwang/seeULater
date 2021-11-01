08c70fab12f64fb49a69c7d146a34702a938b36f
jayhyun-hwang <wogus0501@gmail.com>
2021-11-01 15:02:00 +0900

    clean raact define

--- webApp/sulClient/src/define/define.js
+++ webApp/sulClient/src/define/define.js
@@ -17,8 +17,5 @@ getUrl();
 module.exports = {
     URL: baseurl,
     // URL: "http://127.0.0.1:3001",
-    PORT: 80,
-    REQURL: "http://3.36.36.62",
-    REQURLDEV: "http://127.0.0.1"
-    // REQURL: "http://3.36.36.62:3001"
+    PORT: 80
 }
