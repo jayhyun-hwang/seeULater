927a32c91e6d591a6cbba769b2e856d9b2a15459
jayhyun-hwang <wogus0501@gmail.com>
2021-11-03 12:52:39 +0900

    refactoring chrome ex

--- chromeExtension_dev/background.js
+++ chromeExtension_dev/background.js
@@ -23,66 +23,9 @@ function getStoreClickHandler() {
 
     // The srcUrl property is only available for image elements.
     //var url = 'info.html#' + info.srcUrl;
-    const title = tab.title;
-    const url = tab.url;
-    const iconImg = tab.favIconUrl;
-
-    //object 확인
-    // alert(JSON.stringify(tab));
-
-    // var params = {
-    //   userID: 1,
-    //   url: url
-    // }
-
-    let req = new XMLHttpRequest();
-
-    // var params = "url=" + url;
-
-    // req.open("GET", baseUrl + params, true);
-    // req.send();
-    req.open("POST", baseUrl + "/urls", true);
-    // req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    req.setRequestHeader("Content-type", "application/json");
-    // alert(req);
-    // alert(JSON.stringify(req));
-    req.send(JSON.stringify({
-      url: url,
-      iconImg: iconImg,
-      title: title
-    }));
-    // req.send("userID=1&url=" + url);
-
-    req.onreadystatechange = function () { // Call a function when the state changes.
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        alert("Got response 200!");
-      }
-    }
-    // var urlParams = "userID=1&url=" + url;
-
-    // var data = new FormData();
-    // data.append('userID', 1);
-    // data.append('url', url);
-    // req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // req.send(JSON.stringify({ userID: 1, url: url }));
-
-
-    // Create a new window to the info page.
-    // console.log("title= " + title);
-    // console.log("url= " + url);
-
-    // alert(title + "/" + url);
-    // alert("stored!");
-
-    // chrome.storage.sync.set({ title: url }, function () {
-    //   alert('settings saved' + url);
-    // });
-
-    //chrome.storage.sync.get([title], function(result){
-    //  alert(result.id);
-    //})
-    // alert(chrome.loginState.SessionState);
-    //chrome.windows.create({ url: url, width: 520, height: 660 });
+    submitPostUrls(tab);
+    // //object 확인
+    // // alert(JSON.stringify(tab));
   };
 };
 /**
@@ -100,25 +43,30 @@ chrome.commands.onCommand.addListener(function (command, tab) {
   console.log('onCommand event received for message: ', command);
   if (command == "store_page") {
     console.log('in', command);
-    
-    const title = tab.title;
-    const url = tab.url;
-    const iconImg = tab.favIconUrl;
 
-    let req = new XMLHttpRequest();
-
-    req.open("POST", baseUrl + "/urls", true);
-    req.setRequestHeader("Content-type", "application/json");
-    req.send(JSON.stringify({
-      url: url,
-      iconImg: iconImg,
-      title: title
-    }));
-
-    req.onreadystatechange = function () { // Call a function when the state changes.
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        alert("Got response 200!");
-      }
-    }
+    submitPostUrls(tab);
   }
 });
+
+function submitPostUrls(tab) {
+  
+  let req = new XMLHttpRequest();
+
+  const title = tab.title;
+  const url = tab.url;
+  const iconImg = tab.favIconUrl;
+
+  req.open("POST", baseUrl + "/urls", true);
+  req.setRequestHeader("Content-type", "application/json");
+  req.send(JSON.stringify({
+    url: url,
+    iconImg: iconImg,
+    title: title
+  }));
+
+  req.onreadystatechange = function () { // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      alert("Got response 200!");
+    }
+  }
+}
