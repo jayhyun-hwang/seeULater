7c0cc612152f7d63f8750a9a14eae3c01c2110e4
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-12-09 01:58:49 +0900

    build app for deployee

--- webApp/nodeServer/index.js
+++ webApp/nodeServer/index.js
@@ -44,12 +44,14 @@ app.use(cors());
 const helmet = require("helmet");
 app.use(helmet());
 app.disable("x-powered-by");
-app.use(helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-        "img-src": ["*"]
-    }
-}));
+app.use(
+    helmet.contentSecurityPolicy({
+      useDefaults: true,
+      directives: {
+        "img-src": ["*", "'self'", "data:"],
+      },
+    })
+  );
 
 //add json modules to app
 app.use(express.json());
