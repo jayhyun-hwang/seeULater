fafcdb9d3e13516af0fa02558285ee8dd3c2e0d2
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-12-11 18:23:37 +0900

    build app for deployee

--- buildForDeployee.bat
+++ buildForDeployee.bat
@@ -9,8 +9,9 @@ move build ..\nodeServer\
 @echo ===================
 @timeout /t 1
 
-:: git add --all
-:: git commit -m "build app for deployee"
-:: git push
+cd .\webApp\nodeServer\
+git add --all
+git commit -m "build app for deployee"
+git push
 
 pause
